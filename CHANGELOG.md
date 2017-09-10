e34607f51c3088b2c9fee3cc0bb097de9ebe1c02
Josantonius <info@josantonius.com>
2017-09-10 21:15:51 +0200

    Updated to 1.1.3 version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,23 +1,51 @@
 # CHANGELOG
 
+## 1.1.3 - 2017-09-10
+
+* Unit tests supported by `PHPUnit` were added.
+
+* Added `Algorithm/src/bootstrap.php` file
+
+* Added `Algorithm/tests/bootstrap.php` file.
+
+* Added `Algorithm/phpunit.xml.dist` file.
+* Added `Algorithm/_config.yml` file.
+* Added `Algorithm/.travis.yml` file.
+
+* Deleted `Josantonius\Algorithm\Tests\AlgorithmTest` class.
+* Deleted `Josantonius\Algorithm\Tests\AlgorithmTest::testLookAndSay()` method.
+* Deleted `Josantonius\Algorithm\Tests\AlgorithmTest::testLookAndSayCustom()` method.
+
+* Added `Josantonius\Algorithm\Test\AlgorithmTest` class.
+* Added `Josantonius\Algorithm\Test\AlgorithmTest::testLookAndSay()` method.
+* Added `Josantonius\Algorithm\Test\AlgorithmTest::testLookAndSayCustom()` method.
+* Added `Josantonius\Algorithm\Test\AlgorithmTest::testSequenceError()` method.
+* Added `Josantonius\Algorithm\Test\AlgorithmTest::testLineLimitsError()` method.
+
 ## 1.1.2 - 2017-07-15
+
 * Deleted `Josantonius\Algorithm\Exception\AlgorithmException` abstract class.
 * Deleted `Josantonius\Algorithm\Exception\AlgorithmException->__construct()` method.
 
 ## 1.1.1 - 2017-03-18
+
 * Some files were excluded from download and comments and readme files were updated.
 
 ## 1.1.0 - 2017-01-30
+
 * Compatible with PHP 5.6 or higher.
 
 ## 1.0.0 - 2017-01-30
+
 * Compatible only with PHP 7.0 or higher. In the next versions, the library will be modified to make it compatible with PHP 5.6 or higher.
 
 ## 1.0.0 - 2017-01-21
+
 * Added `Josantonius\Algorithm\Algorithm` class.
 * Added `Josantonius\Algorithm\Algorithm::lookAndSay()` method.
 
 ## 1.0.0 - 2017-01-21
+
 * Added `Josantonius\Algorithm\Exception\AlgorithmException` abstract class.
 * Added `Josantonius\Algorithm\Exception\AlgorithmException->__construct()` method.
 
---
+++ tests/Algorithm/Test/AlgorithmTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Class with algorithms to solve and display mathematical sequences.
+ * 
+ * @author     Josantonius - hello@josantonius.com
+ * @copyright  Copyright (c) 2017
+ * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
+ * @link       https://github.com/Josantonius/PHP-Algorithm
+ * @since      1.1.3
+ */
+
+namespace Josantonius\Algorithm\Test;
+
+use Josantonius\Algorithm\Algorithm,
+    PHPUnit\Framework\TestCase;
+/**
+ * Tests class for Algorithm library.
+ *
+ * @since 1.1.3
+ */
+final class AlgorithmTest extends TestCase { 
+
+    /**
+     * Show 15 lines of the Look-and-Say sequence starting with 1.
+     *
+     * @since 1.1.3
+     */
+    public function testLookAndSay() {
+
+        $str = Algorithm::lookAndSay();
+
+        $this->assertInternalType('string', $str);
+
+        // Only numbers between 1 and 3 must exist
+
+        $this->assertTrue(preg_match('/^[1-3\s]+$/', $str) === 1);
+    }
+
+    /**
+     * Customize initial value and lines to show the Look-and-Say sequence.
+     *
+     * @since 1.1.3
+     */
+    public function testLookAndSayCustom() {
+
+        $str = Algorithm::lookAndSay(8, 15);
+
+        $this->assertInternalType('string', $str);
+
+        // Only the number 8 and numbers between 1 and 3 must exist
+
+        $this->assertTrue(preg_match('/^[1-38\s]+$/', $str) === 1);
+    }
+
+    /**
+     * Failed to use negative values in sequence.
+     *
+     * @since 1.1.3
+     */
+    public function testSequenceError() {
+
+        $this->assertFalse(Algorithm::lookAndSay(-1, 15));
+    }
+
+    /**
+     * Failed to use negative values in line limits.
+     *
+     * @since 1.1.3
+     */
+    public function testLineLimitsError() {
+
+        $this->assertFalse(Algorithm::lookAndSay(1, -15));
+    }
+}
